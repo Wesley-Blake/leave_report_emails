fd53da4f22559e9d27d6f20fee2973a767f11f7d
wesblake@proton.me <wesblake@proton.me>
2026-07-31 14:44:49 -0700

    Refactor: added configparser instead of open(). Add: ruff formatting.

--- src/leave_report_emails/main.py
+++ src/leave_report_emails/main.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 import pandas as pd
 import validators
-
 from win32com_email import win_email
 
 
--- src/leave_report_emails/win32com_email.py
+++ src/leave_report_emails/win32com_email.py
@@ -1,6 +1,11 @@
+import configparser
 from pathlib import Path
 
 import win32com.client as win32
+
+cfg = configparser.ConfigParser()
+cfg.read(Path(__name__).parent / ".env")
+LINK = Path(cfg["leave_report_emails"]["how_to_submit"])
 
 
 def win_email(cc: str, bcc: list[str], pay_period: str, body: str) -> None:
@@ -13,10 +18,8 @@ def win_email(cc: str, bcc: list[str], pay_period: str, body: str) -> None:
     mail.CC = cc
     mail.BCC = "; ".join(bcc)
     mail.Subject = f"Leave Report Month {pay_period}"
-    with open("leave_report_emails\\secrets.txt", "r") as file:
-        attachment = Path(file.readline().strip())
-    if attachment.is_file():
-        mail.Attachments.Add(str(attachment))
+    if LINK.is_file():
+        mail.Attachments.Add(str(LINK))
     mail.Body = body
     # mail.Display()
     mail.Send()
